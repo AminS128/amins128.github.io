d96ddec17867dcb763d3144277cf886bb24b7dbd
Amin Smith <aminsmith128@gmail.com>
2025-10-07 15:06:15 -0400

    first version of page
    need to upload typewriter, put it on

---
+++ main.js
@@ -0,0 +1,31 @@
+function resizeMiddle(){
+    document.getElementById('main-center').style.width = Math.min(800, (document.body.clientWidth * 0.8))+ "px"
+}
+
+window.addEventListener('resize', resizeMiddle)
+document.addEventListener('DOMContentLoaded', resizeMiddle)
+
+const pages = [
+    ["a particle based simulator for echolocation", "https://amins128.github.io/echo", "Echo"],
+    ["a utility for text-based display", "https://amins128.github.io/misc/typewriter.js", "typewriter.js"],
+    ["a Chrome extension that detects AI images", "https://github.com/amins128/aichecker", "Hopecheck"],
+    ["an AI image detector benchmarking site", "https://amins128.github.io/aichecker/bench", "aichecker/bench"],
+]
+
+document.addEventListener('DOMContentLoaded', ()=>{
+    let e = document.getElementById('link-container')
+    pages.forEach((v)=>{
+        let a = document.createElement('a')
+        let s = document.createElement('span')
+        let p = document.createElement('p')
+        a.className = "typewriter"
+        s.className = "typewriter"
+        a.href = v[1]
+        a.textContent = v[2]
+        s.textContent = " - " + v[0]
+        p.appendChild(a)
+        p.appendChild(s)
+        // d.innerHTML += "<br>"
+        e.appendChild(p)
+    })
+})
